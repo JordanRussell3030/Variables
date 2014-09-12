05c35cf3a87ac47b5810a8b34b4af6f6bf71f5f2
44240 <44240@PC004150.coll.lr.local>
2014-09-12 14:07:21 +0100

    Adding 4 numbers together

---
+++ Addition task.py
@@ -0,0 +1,10 @@
+#Jordan Russell
+#12/09/14
+#Addition task
+
+first_integer = int(input("Please input your first number: "))
+second_integer = int(input("Please input your second number: "))
+third_integer = int(input("Please input your third number: "))
+fourth_integer = int(input("Please input your fourth number: "))
+result = (first_integer + second_integer + third_integer + fourth_integer)
+print("Your four numbers add to make {0}".format(result))
