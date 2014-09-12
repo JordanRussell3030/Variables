a2d06680cdfe81c54c38b0f202123c6cae53c4b2
44240 <44240@PC004150.coll.lr.local>
2014-09-12 14:11:24 +0100

    Multiplying two numbers then dividing the result by a third number.

---
+++ Multiplying then dividing task.py
@@ -0,0 +1,10 @@
+#Jordan Russell
+#12/09/14
+#Multiplication and division task
+
+first_integer = int(input("Please input your first number: "))
+second_integer = int(input("Please input your second number: "))
+third_integer = int(input("Please input your third number: "))
+fourth_number = (first_integer * second_integer)
+result = (fourth_number / third_integer)
+print("The result of multiplying the first two numbers and dividing the result by your third number is {0}".format(result))
