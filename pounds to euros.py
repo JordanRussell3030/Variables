fa19e55f92b5120c5cd14a20b357517987971c89
Jordan <Jordan@Jordanslaptop.home>
2014-09-17 19:17:23 +0100

    Challenge 6 on python school converting sterling to euros.

---
+++ pounds to euros.py
@@ -0,0 +1,9 @@
+#Jordan Russell
+#17/09/14
+#Python School math unit exercise 6
+
+money_sterling = float(input("Please input the amount of money in sterling you are taking on holiday: "))
+money_euros = round(money_sterling * 1.26)
+
+print("You will receive {0} euros".format(money_euros))
+
