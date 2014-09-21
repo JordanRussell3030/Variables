15eafba421baa842826b48fa42becb9e749a73f8
Jordan <Jordan@192.168.1.95>
2014-09-21 15:22:16 +0100

    The second selection R+R task, tells the user whether or not the number they input is within a range of numbers.

---
+++ IF statements R+R 2.py
@@ -0,0 +1,11 @@
+#Jordan Russell
+#21/09/14
+#Selection R+R task 2
+
+number = int(input("Please input a number between 1 and 20: "))
+if (number < 1):
+    print("This number is invalid, it is too low.")
+if (number > 20):
+    print("This number is invalid, it is too high.")
+else:
+    print("This number is within the range.")
