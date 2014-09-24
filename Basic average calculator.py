ee3009a4be6a809f9165234d720670cae55c381d
Jordan <Jordan@Jordan-PC.home>
2014-09-24 19:31:37 +0100

    Basic average calculator which will be improved over time.

---
+++ Basic average calculator.py
@@ -0,0 +1,12 @@
+#Jordan Russell
+#24/09/14
+#Average calculator
+
+print("This program will calculate an average from five numbers which you input.")
+integer_1 = int(input("Please input your first integer: "))
+integer_2 = int(input("Please input your second integer: "))
+integer_3 = int(input("Please input your third integer: "))
+integer_4 = int(input("Please input your fourth integer: "))
+integer_5 = int(input("Please input your fifth integer: "))
+result = (integer_1 + integer_2 + integer_3 + integer_4 + integer_5) / 5
+print("The average of your numbers is {0}.".format(result))
